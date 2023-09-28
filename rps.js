03c65a995acee6e129126b1daf20a90d4095391b
Sharon Ozumba <ozumbaezinne@gmail.com>
2023-09-28 17:44:48 +0100

    Complete rps game to end at 5 rounds

--- rps.js
+++ rps.js
@@ -1,13 +1,16 @@
 const container = document.querySelector('#container')
 
+// Buttons 
 const btn1 = document.querySelector('.rock')
-
 const btn2 = document.querySelector('paper')
-
 const btn3 = document.querySelector('scissors')
 
-const div = document.createElement("div");
-container.appendChild(div);
+// Selectors for Other Elements
+const player = document.querySelector('.player');
+const computer = document.querySelector('.computer');
+const roundScore = document.querySelector('.round-score');
+const finalScore = document.querySelector('.final-score');
+const newGame = document.querySelector('.new-game');
 
 let choices = ["Rock", "Paper", "Scissors"];
 
@@ -15,24 +18,38 @@ function getComputerChoice() {
   return choices[Math.floor(Math.random() * 3)];
 }
 
+// Set Variables 
+let playerScore = 0;
+let computerScore = 0;
+let isGameOver = false;
+const scoreToReach = 5;
+
 function playRound(playerSelection, computerSelection) {
   let ps = playerSelection.toLowerCase();
   let cs = computerSelection.toLowerCase();
-
+  const newLi = document.createElement('li')
+  newLi.classList.add('newLi')
   if (
     (ps == "paper" && cs == "rock") ||
     (ps == "scissors" && cs == "paper") ||
     (ps == "rock" && cs == "scissors")
   ) {
-    return "Player!";
+    newLi.textContent = `You played ${ps} and Computer played ${cs}. You win!`;
+    roundScore.appendChild(newLi);
+    playerScore++
+    player.textContent = playerScore;
   } else if (
     (cs == "paper" && ps == "rock") ||
     (cs == "scissors" && ps == "paper") ||
     (cs == "rock" && ps == "scissors")
   ) {
-    return "Computer!";
+    newLi.textContent = `You played ${ps} and Computer played ${cs}. You lose!`;
+    roundScore.appendChild(newLi);
+    computerScore++;
+    computer.textContent = computerScore;
   } else {
-    return "Tie";
+    newLi.textContent = `You played ${ps} and Computer played ${cs}. That's a tie!`;
+    roundScore.appendChild(newLi);
   }
 }
 
@@ -40,14 +57,35 @@ function playRound(playerSelection, computerSelection) {
 const btns = document.querySelectorAll('button')
 btns.forEach((button) => {
   let choice = button.textContent;
-  let rounds = 5
-  let winner = 0
-  let loser = 0
   button.addEventListener("click", () => {
-    div.textContent = playRound(choice, getComputerChoice());
+    if (!isGameOver) {
+      playRound(choice, getComputerChoice());
+      endGame();
+    }
   });
 });
 
+newGame.addEventListener('click', () => {
+  isGameOver = false;
+  playerScore = 0;
+  computerScore = 0;
+  roundScore.textContent = '';
+  player.textContent = '0';
+  computer.textContent = '0';
+  finalScore.textContent = '';
+})
+
+function endGame() {
+  if (playerScore === scoreToReach || computerScore === scoreToReach) {
+    isGameOver = true;
+    
+    if (playerScore === scoreToReach) {
+      finalScore.textContent = 'You won! Yippeeee!';
+    } else if (computerScore === scoreToReach) {
+      finalScore.textContent = 'You lost! Bummer 😞'
+    }
+  }
+}
 
 
 
